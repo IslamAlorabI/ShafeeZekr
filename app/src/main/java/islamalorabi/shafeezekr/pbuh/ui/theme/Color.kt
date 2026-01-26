1b5d7c64a58c2aeb9d9819dadbc92430a1c35701
Islam AlorabI <em180423@gmail.com>
2026-01-26 18:11:04 +0200

    feat: implement new card style for home screen

--- app/src/main/java/islamalorabi/shafeezekr/pbuh/ui/theme/Color.kt
+++ app/src/main/java/islamalorabi/shafeezekr/pbuh/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// New Card Style Colors
+val CardBluePrimary = Color(0xFF134563) // Approx from image
+val CardDarkSurface = Color(0xFF2C323D) // Approx from image
+val CardBackground = Color(0xFF0F1216) // Darker background
+val CardTextPrimary = Color(0xFFFFFFFF)
+val CardTextSecondary = Color(0xFFB0B3B8)
+val CardAccent = Color(0xFF64B5F6)
