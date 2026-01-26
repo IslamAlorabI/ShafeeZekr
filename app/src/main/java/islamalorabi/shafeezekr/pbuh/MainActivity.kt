892182685a165a235b09023369492bb70ed57fa2
Islam AlorabI <em180423@gmail.com>
2026-01-26 14:31:59 +0200

    fix: resolve AppCompat crash by updating theme and apply A9 fixes

--- app/src/main/java/islamalorabi/shafeezekr/pbuh/MainActivity.kt
+++ app/src/main/java/islamalorabi/shafeezekr/pbuh/MainActivity.kt
@@ -1,12 +1,10 @@
 package islamalorabi.shafeezekr.pbuh
 
-import android.app.LocaleManager
 import android.os.Build
 import android.os.Bundle
-import android.os.LocaleList
-import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
